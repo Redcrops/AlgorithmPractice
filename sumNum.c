bf58c65fc50ff6e723967e6405ac812d860f7121
吕子衡 <you@example.com>
2023-12-10 21:51:15 +0800

    [新增]两数相加

---
+++ sumNum.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define NUM_SIZE 5
+#define RANGE_NUM 10
+#define RET_SIZE 2
+void sum_Num(int array[NUM_SIZE], int target, int ret[RET_SIZE])
+{
+
+    int sum_Num = 0;
+    for (int idx = 0; idx < NUM_SIZE - 1; idx++)
+    {
+        for (int jdx = 1; jdx < NUM_SIZE - idx; jdx++)
+        {
+            if (array[idx] + array[jdx] == target)
+            {
+                ret[0] = idx;
+                ret[1] = jdx;
+                return;
+            }
+        }
+    }
+    ret[0] = -1;
+    ret[1] = -1;
+}
+int main()
+{
+    int array[NUM_SIZE];
+    int target;
+    int ret[RET_SIZE];
+    srand(time(NULL));
+    for (int idx = 0; idx < NUM_SIZE; idx++)
+    {
+        array[idx] = rand() % RANGE_NUM + 1;
+    }
+    for (int jdx = 0; jdx < NUM_SIZE; jdx++)
+    {
+        printf("array[%d]=%d\n", jdx, array[jdx]);
+    }
+    printf("input your target num:\n");
+    scanf("%d", &target);
+    sum_Num(array, target, ret);
+    if ((ret[0] == -1) && (ret[1] == -1))
+    {
+        printf("no result you want !\n");
+    }
+    else
+    {
+        printf("(%d,%d)\n", ret[0], ret[1]);
+    }
+
+    return 0;
+}
